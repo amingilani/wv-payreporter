9ad69c6a3b31148dfaa8f763fe974db9d5b456bf
Amin Shah Gilani <amin@gilani.me>
2019-02-01 22:26:26 +0500

    Add routes to render csv and json

--- payreporter/app/controllers/payroll_report_controller.rb
+++ payreporter/app/controllers/payroll_report_controller.rb
@@ -1,5 +1,15 @@
+
 class PayrollReportController < ApplicationController
   def index
-    @payroll_report = PayrollReporter.call
+    @payroll_report = PayrollManager::Reporter.call
+    respond_to do |format|
+      format.html
+      format.json { render json: PayrollManager::ReportJsonFormatter.call(@payroll_report) }
+      format.csv do
+        headers['Content-Disposition'] = "attachment; filename=\"payroll-report-#{Time.zone.now.to_date}.csv\""
+        headers['Content-Type'] ||= 'text/csv'
+        render inline: '<%= PayrollManager::ReportCsvFormatter.call(@payroll_report) %>'
+      end
+    end
   end
 end
--- payreporter/config/routes.rb
+++ payreporter/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root 'payroll_report#index'
+  get 'payroll_report', to: 'payroll_report#index', defaults: { format: 'json' }
+
   resource :time_reports
 end
