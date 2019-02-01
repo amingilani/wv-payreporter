83316ba8f5f099a3042874d77771171d4db274df
Amin Shah Gilani <amin@gilani.me>
2019-02-02 04:55:12 +0500

    Refactor tests

--- payreporter/test/controllers/time_reports_controller_test.rb
+++ payreporter/test/controllers/time_reports_controller_test.rb
@@ -3,36 +3,47 @@ require 'csv'
 
 class TimeReportsControllerTest < ActionDispatch::IntegrationTest
   test 'creating a valid and trying to create an invalid time report' do
-    assert_difference 'TimeLog.count', 32 do
-      post time_reports_url,
-           params: {
-             file: fixture_file_upload(
-               Rails.root.join('test', 'fixtures', 'files', 'sample.csv'),
-               'text/csv'
-             )
-           }
-    end
+    successfully_import_a_time_report
+    fail_to_upload_the_same_time_report_again
+    download_a_csv_payroll_report
+    view_a_json_payroll_report
+  end
+
+  private
+
+  def successfully_import_a_time_report
+    assert_difference('TimeLog.count', 32) { import_a_time_report }
     assert_response :redirect
     follow_redirect!
     assert_response :success
     assert_select 'div', 'Time report successfully saved.'
+  end
 
-    assert_difference 'TimeLog.count', 0 do
-      post time_reports_url,
-           params: {
-             file: fixture_file_upload(
-               Rails.root.join('test', 'fixtures', 'files', 'sample.csv'),
-               'text/csv'
-             )
-           }
-    end
+  def fail_to_upload_the_same_time_report_again
+    assert_difference('TimeLog.count', 0) { import_a_time_report }
     follow_redirect!
     assert_select 'div', 'A time report with this report id was already imported.'
+  end
 
+  def download_a_csv_payroll_report
     get payroll_report_url(format: :csv)
     assert_response :success
+  end
 
+  def view_a_json_payroll_report
     get payroll_report_url(format: :json)
     assert_response :success
+  end
+
+  ## helper methods below
+
+  def import_a_time_report
+    post time_reports_url,
+         params: {
+           file: fixture_file_upload(
+             Rails.root.join('test', 'fixtures', 'files', 'sample.csv'),
+             'text/csv'
+           )
+         }
   end
 end
