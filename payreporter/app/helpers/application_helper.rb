1112635efba89601620165ffdaffa53f349cf4e0
Amin Shah Gilani <amin@gilani.me>
2019-02-02 18:04:30 +0500

    Add simple form, and refactor & beautify the views

--- payreporter/app/helpers/application_helper.rb
+++ payreporter/app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def flash_class(level)
+    case level
+    when 'alert' then 'ui red message'
+    when 'notice' then 'ui blue message'
+    else
+      'ui message'
+    end
+  end
 end
